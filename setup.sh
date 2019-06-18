95a063ea2bae56d032913b81f0f69720b23331d1
root <root@yokolabo>
2019-06-18 14:29:44 +0900

    rm old_main
    不要なmainシェルを削除

--- setup.sh
+++
@@ -1,5 +0,0 @@
-./init.txt
-./init.txt
-route add -net 192.168.1.0 gw 192.168.3.2 netmask 255.255.255.0
-route add -net 192.168.2.0 gw 192.168.4.2 netmask 255.255.255.0
-route add -net 192.168.15.0 gw 192.168.4.2 netmask 255.255.255.0
