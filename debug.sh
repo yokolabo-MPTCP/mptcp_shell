3f5f8b87378f1f612dbcfcd0f4801311eaeccfea
Daichi Izumi <izumi.net@s.okayama-u.ac.jp>
2019-06-28 17:29:54 +0900

    [add] log file for debug
    デバッグ用のログファイルの追加

---
+++ debug.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -e "function.sh" ]; then
+    source "function.sh"
+else
+    echo "function.sh dose not exist."
+    exit
+fi
+
+cgn_ctrl=(lia)         
+rtt1=(50)              
+rtt2=(50)              
+loss=(0)               
+queue=(100) 
+duration=100           
+app_delay=0.5          
+repeat=1               
+app=3                  
+subflownum=2
+qdisc=pfifo_fast       
+memo=$1   
+today=debug_log
+split_log
